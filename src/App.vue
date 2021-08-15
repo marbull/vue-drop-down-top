e8c4e44874646807c130086732b70a799df05b1e
marbull <madawasashi@gmail.com>
2021-08-15 22:18:34 +0900

    add: style at Navigation.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <Header />
+  <Navigation />
 </template>
 
 <script setup>
-  import Header from './components/Header.vue';
+  import Navigation from './components/Navigation.vue';
 </script>
